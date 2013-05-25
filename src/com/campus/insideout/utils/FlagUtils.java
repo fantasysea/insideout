497c51f8c69ddce8cc5bf61092e03bcda5e9f187
fantasysea <weishaoyin2011@gmail.com>
2013-05-25 21:20:31 +0800

    remove

--- src/com/campus/insideout/utils/FlagUtils.java
+++
@@ -1,8 +0,0 @@
-package com.campus.insideout.utils;
-
-public class FlagUtils {
-
-	public static final String SCHEMA = "moxian://android_move";
-	public static final String PARAM_UID = "uid";
-	
-}
